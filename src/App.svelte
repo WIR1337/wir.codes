42da7e99b5f9b39bfb17bee8f3cbe17c5caeb66a
Alexandr N <naberezhnyy.an@gmail.com>
2023-01-06 21:07:55 +0100

    add eurrub

--- src/App.svelte
+++ src/App.svelte
@@ -16,14 +16,41 @@
     });
     console.log("currencyPrice : ", $currencyPrice);
   }
+
+  let eurrub = {
+    symbol: "EURRUB",
+  };
+
+  $: {
+    if ($currencyPrice.length != 0) {
+      eurrub.price = $currencyPrice[0].price * $currencyPrice[1].price;
+    }
+  }
 </script>
 
 <main>
   <div>Wellcome on wir.codes !</div>
+  <button on:click={setPrice}>Get Data</button>
 
-  <button on:click={setPrice}>Log Res</button>
-
-  <div>
-    {$currencyPrice[0]?.price ? $currencyPrice[0]?.price : "Data still not yet"}
-  </div>
+  {#if eurrub.price}
+    <div class="currency">
+      <div>{eurrub.symbol}</div>
+      <div>{Number(eurrub.price).toFixed(2)}</div>
+    </div>
+  {/if}
+  {#each $currencyPrice as { symbol, price }}
+    <div class="currency">
+      <div>{symbol}</div>
+      <div>{Number(price).toFixed(2)}</div>
+    </div>
+  {:else}
+    <div>No data</div>
+  {/each}
 </main>
+
+<style>
+  .currency {
+    display: flex;
+    gap: 8px;
+  }
+</style>
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 
 
-export let currencyPrice = writable({})
+export let currencyPrice = writable([])
