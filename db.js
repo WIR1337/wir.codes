2532dc2c073b1becae04dada6115b4bc60b2abf1
Alexandr N <naberezhnyy.an@gmail.com>
2023-01-11 17:30:19 +0100

    init routing

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 import postgres from 'postgres'
 
 const sql = postgres({
-    host: 'localhost',
-    port: 5432,
-    database: 'mydb',
-    username: 'postgres'
+  host: 'localhost',
+  port: 5432,
+  database: 'mydb',
+  username: 'postgres'
 }) // will use psql environment variables
 
 export async function getUsersOver() {
-    const city = await sql`
+  const city = await sql`
       select
         city
       from weather
     `
-    console.log(city);
-    return city
+  console.log(city);
+  return city
 }
+await getUsersOver()
